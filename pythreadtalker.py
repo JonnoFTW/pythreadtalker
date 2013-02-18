6d00b89e36f68293c61cc7c1e02e29de1e93108f
Jonathan Mackenzie <jonmac1@gmail.com>
2013-02-18 13:47:31 -0800

    Finished
    Says a thread properly. The voice is annoying though.

--- pythreadtalker.py
+++ pythreadtalker.py
@@ -1,5 +1,21 @@
-from espeak import espeak
-from datetime import datetime
-import jsonlib
+import json, urllib2, re, pyttsx, time, sys, HTMLParser
+board = raw_input("BOARD: ")
+thread = raw_input("THREAD: ")
+engine = pyttsx.init()
+h = HTMLParser.HTMLParser()
+def saytext(t):
+    t = re.sub('<[^<]+?>', ' ', h.unescape(t))
+    print  t
+    engine.say(t)
+    engine.runAndWait()
+url = "http://api.4chan.org/%s/res/%s.json"%(board,thread)
+oldest = 0
+while True:
+    page = json.load(urllib2.urlopen(url))
+    for i in page['posts']:
+        if i['time'] > oldest and 'com' in i:
+            saytext(i["com"])
+        oldest = i['time']
+    time.sleep(20)
 
-http://api.4chan.org/g/res/31546867.json
+    
